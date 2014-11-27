7aa6f5383dde3f9eaeb6b8cb7c68053d4adb9789
dentex <samuele.rini76@gmail.com>
2014-11-27 18:06:02 +0100

    gio 27 nov 2014, 18.06.02, CET

--- pi_home_scripts/bin/dweet_to_freeboard.sh
+++ pi_home_scripts/bin/dweet_to_freeboard.sh
@@ -21,7 +21,9 @@ function getCpuTemp {
 
 function cats {
   cputemp=`getCpuTemp`
-  watertemp=`/home/pi/bin/get_water_temp.sh`
+  gputemp=`echo $(/opt/vc/bin/vcgencmd measure_temp) | grep -oE '[0-9][0-9].[0-9]'`
+
+  watertemp=`/home/pi/bin/get_water_temp.sh | grep -oE '[0-9][0-9].[0-9]'`
 
   t8=`cat /home/pi/log/last_t8_status`
   co2=`cat /home/pi/log/last_co2_status`
@@ -30,14 +32,30 @@ function cats {
 }
 
 function dweet {
+  curl -d "status=Running" "http://dweet.io/dweet/for/piac" > /dev/null 2>&1
   curl -d "cpu-temp=$cputemp" "http://dweet.io/dweet/for/piac" > /dev/null 2>&1
+  curl -d "gpu-temp=$gputemp" "http://dweet.io/dweet/for/piac" > /dev/null 2>&1
   curl -d "water-temp=$watertemp" "http://dweet.io/dweet/for/piac" > /dev/null 2>&1
   curl -d "t8-status=$t8" "http://dweet.io/dweet/for/piac" > /dev/null 2>&1
   curl -d "co2-status=$co2" "http://dweet.io/dweet/for/piac" > /dev/null 2>&1
   curl -d "lid-fan-step=$fan" "http://dweet.io/dweet/for/piac" > /dev/null 2>&1
   curl -d "night-leds-level=$leds" "http://dweet.io/dweet/for/piac" > /dev/null 2>&1
+  curl -d "last-update=$(date +%T)" "http://dweet.io/dweet/for/piac" > /dev/null 2>&1
+}
+
+function dweet_off {
+  curl -d "status=Sleeping" "http://dweet.io/dweet/for/piac" > /dev/null 2>&1
 }
 
 check_network
-cats
-dweet
+
+# Hour of the day
+H=`date +%H`
+
+# run only from 08:00 to 23:59
+if [ "$H" -gt 7 ] && [ "$H" -lt 24 ]; then
+  cats
+  dweet
+else
+  dweet_off
+fi
--- pi_home_scripts/bin/get_water_temp.sh
+++ pi_home_scripts/bin/get_water_temp.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 #   probe output ------------------------------------------------ get raw temp -------- print it divided by 1000 and rounded to the 2nd decimal
-cat /sys/bus/w1/devices/w1_bus_master1/28-00000550758e/w1_slave | grep -oE '[0-9]{5}' | awk  '{x=$1}END{rounded = sprintf("%.2f", x/1000); print rounded}'
+t=`cat /sys/bus/w1/devices/w1_bus_master1/28-00000550758e/w1_slave | grep -oE '[0-9]{5}' | awk  '{x=$1}END{rounded = sprintf("%.1f", x/1000); print rounded}'`
+
+echo " temp=$t'C"
